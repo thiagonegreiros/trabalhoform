62dc8fee2458d97b4bad4d92db7fe18326cca45e
Thiago Negreiros <thiago.negreiros@dmz.org.br>
2022-05-10 20:50:55 -0400

    feat: Finish job

---
+++ receive-form.php
@@ -0,0 +1,16 @@
+<?php
+
+$normal1  = $_POST["normal-input-1"]
+$normal2  = $_POST["normal-input-2"]
+$rinput   = $_POST["radio-input"]
+$redi     = $_POST["red"]
+$applei   = $_POST["apple"]
+$bannana  = $_POST["banana"]
+$movie    = $_POST["movie"]
+
+echo "Section 1\n"
+echo "Question 1: $normal1\n"
+echo "Question 2: $normal2\n"
+echo "Radio 2: $rinput\n"
+
+?>
